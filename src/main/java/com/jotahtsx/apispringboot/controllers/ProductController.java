264977cc1196667c30f3070a4eee55b6214c1755
jotahtsx <joaomanoelskai@gmail.com>
2023-12-26 10:39:09 -0300

    feat: productModel getters and setters

---
+++ src/main/java/com/jotahtsx/apispringboot/controllers/ProductController.java
@@ -0,0 +1,8 @@
+package com.jotahtsx.apispringboot.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+    
+}
---
+++ src/main/java/com/jotahtsx/apispringboot/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.jotahtsx.apispringboot.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jotahtsx.apispringboot.models.ProductModel;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+    
+}
