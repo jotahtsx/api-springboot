1703044b448765c90df4e21d86e2d5869a0ded6a
jotahtsx <joaomanoelskai@gmail.com>
2023-12-26 08:48:33 -0300

    Init project

---
+++ src/test/java/com/jotahtsx/apispringboot/ApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jotahtsx.apispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
